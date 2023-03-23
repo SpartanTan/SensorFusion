4a8a233c0aae240b3353e8e7c0498044ac4a8af8
Zhicun <tanzc9866@126.com>
2023-03-24 00:41:15 +0100

    update some stuff in section 1

---
+++ ChM015x_SensorFusion/Section 1/oldjointGaussian.m
@@ -0,0 +1,21 @@
+function [mu, Sigma] = oldjointGaussian(mu_x, sigma2_x, sigma2_r)
+%jointGaussian calculates the joint Gaussian density as defined
+%in problem 1.3a. 
+%
+%Input
+%   MU_X        Expected value of x
+%   SIGMA2_X    Covariance of x
+%   SIGMA2_R    Covariance of the noise r
+%
+%Output
+%   MU          Mean of joint density 
+%   SIGMA       Covariance of joint density
+
+
+%Your code here
+A = [1 0;1 1];
+b = 0;
+mu_x = [mu_x;0];
+Sigma_x = blkdiag(sigma2_x,sigma2_r);
+[mu, Sigma] = affineGaussianTransform(mu_x, Sigma_x, A, b);
+end
--- ChM015x_SensorFusion/Section 1/sensor.m
+++ ChM015x_SensorFusion/Section 1/sensor.m
@@ -1,4 +1,7 @@
 function  ys = sensor(xs)
+%Input
+%   xs  Object position [n*1]
+
     range = sqrt(xs(1,:).^2+xs(2,:).^2);    
     angle = atan2(xs(2,:),xs(1,:));
     ys = [range;angle];
